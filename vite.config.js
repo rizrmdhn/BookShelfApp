81ea0aa7facc6122d65f4683473657e503b25c66
rizrmdhn <rizrmdhn.work@gmail.com>
2022-11-17 13:50:07 +0800

    change host

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: process.env.PORT
+    host: true,
+    port: process.env.PORT,
   },
   base: '/BookShelfApp/',
   plugins: [react()]
